bedf7c2a80d4d4b65109a611458df1763961236a
MarcelloAllegriUNIPR <74766893+MarcelloAllegriUNIPR@users.noreply.github.com>
2021-11-29 21:19:35 +0100

    parametrizzazione cicli in base al nr di thread

--- src_variables/variable_2Dgeneral.f90
+++ src_variables/variable_2Dgeneral.f90
@@ -2,7 +2,7 @@ MODULE variable_2Dgeneral
     
     use cudafor
     !-- gpu --
-    integer :: useGpu = 0
+    integer :: useGpu = 1
 
     !!! FILE !!!
     CHARACTER(len=200)::  dir, ext
--- src_variables/variables.f90
+++ src_variables/variables.f90
@@ -8,6 +8,6 @@ module Variables_DP
     double precision, device, allocatable, dimension(:,:) :: punto_m_1, punto_m_2, punto_m_tilde_1, punto_m_tilde_2, CA, CD, CBCFCECC, CFCACCCD, &
                                                              CBCDCECA, CBCCCECF, CACCCFCD, CACBCDCE, Vu_d, matrix_d
     logical, device, allocatable, dimension(:) :: IsULeftAssI, IsULeftAssJ
-    integer, device :: delta_kronecker_d(2,2), grado_q_d, DimVu_d, hk_d
+    integer, device :: delta_kronecker_d(2,2), grado_q_d, DimVu_d, hk_d, NGauss_d
     integer, device, allocatable, dimension(:,:) :: flag_extra, list_elements_d
 end module Variables_DP
