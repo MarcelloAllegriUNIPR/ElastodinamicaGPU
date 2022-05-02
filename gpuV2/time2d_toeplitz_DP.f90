b8112834c16c876e9e36c0fae819000262a80035
MarcelloAllegriUNIPR <74766893+MarcelloAllegriUNIPR@users.noreply.github.com>
2022-05-02 13:30:39 +0200

    profiling e miglioramenti pt 1

--- gpuV2/time2d_toeplitz_DP.f90
+++ gpuV2/time2d_toeplitz_DP.f90
@@ -1,7 +1,8 @@
 SUBROUTINE time2D_toeplitz_DP(i_time,file_output)
 
+    use cudafor
     USE variable_2Dgeneral
-    use kernel_MakeVuBlocco
+    use kernel
     !USE OMP_LIB
     IMPLICIT NONE
 
@@ -11,7 +12,7 @@ SUBROUTINE time2D_toeplitz_DP(i_time,file_output)
     INTEGER(kind=4),INTENT(IN):: i_time
     CHARACTER(len=200),INTENT(IN):: file_output
     !Variabili locali
-    INTEGER(kind=4):: i, j, iblocco, jblocco, iriga, icolonna, hk, indice_i, indice_j,AllocateStatus
+    INTEGER(kind=4):: i, j, iblocco, jblocco, iriga, icolonna, hk, indice_i, indice_j,AllocateStatus,istat,sizeofShMem
     !REAL(kind=8),DIMENSION(grado_q+1,grado_q+1):: blocco
     double precision :: var
     REAL(kind=8),DIMENSION(:,:),ALLOCATABLE:: Vu
@@ -32,18 +33,27 @@ SUBROUTINE time2D_toeplitz_DP(i_time,file_output)
     hk_d = i_time-1
     grid = dim3(2*DimVu,2*DimVu,1)
     !grid = dim3(1,3,1)
-    tBlock = dim3(N_gauss,8,1)
+    tBlock = dim3(N_gauss,N_gauss/4,1)
     
-    call Make_Vu_Blocco_DP<<<grid,tblock,(sizeof(var)*(N_gauss+1)*8)>>>(hk_d) !+sizeof(var)*5+sizeof(hk)*4
+    sizeofShMem = sizeof(var)*(N_gauss+1)*N_gauss/4
+    call cudaProfilerStart();
+    call Make_Vu_Blocco_DP<<<grid,tblock,sizeofShMem>>>(hk_d) 
+
+    istat= cudaGetLastError()
+    if(istat /= cudaSuccess) write(*,*) 'Sync kernel error: ' , cudaGetErrorString(istat)
+    istat= cudaDeviceSynchronize()    
+    if(istat /= cudaSuccess) write(*,*) 'Async kernel error : ', cudaGetErrorString(istat)    
 
     matrix = matrix_d    
-    !print *, matrix(150,1:10)
-    !DEALLOCATE(Vu_d)
-    !DEALLOCATE(matrix_d)
+    istat = cudaDeviceReset()
+    call cudaProfilerStop();
+
     WRITE (i_time_st,fmt) i_time
   
-  file_mat=TRIM(file_output) // '_matrice_' // TRIM(i_time_st)
-   CALL filepath(file_path_mat,dir,file_mat,ext)  
+    file_mat=TRIM(file_output) // '_matrice_' // TRIM(i_time_st)
+    CALL filepath(file_path_mat,dir,file_mat,ext)  
+    CALL filepath(file_path_mat,dir,file_mat,ext)  
+    CALL filepath(file_path_mat,dir,file_mat,ext)  
      OPEN(i_time+6,FILE=TRIM(file_path_mat))
         DO i=1,2*DimVu      
           DO j=1,2*DimVu    
@@ -53,7 +63,8 @@ SUBROUTINE time2D_toeplitz_DP(i_time,file_output)
          END DO
         END DO
       CLOSE(i_time+6)
-RETURN
+    
+ RETURN
   
 END SUBROUTINE time2D_toeplitz_DP
   
--- src_variables/variables.f90
+++ src_variables/variables.f90
@@ -2,12 +2,12 @@ module Variables_DP
 
     implicit none
 
-    double precision, device :: cs_d, cp_d, rho_d, delta_t_d
+    double precision, constant :: cs_d, cp_d, rho_d, delta_t_d
     double precision, device, allocatable, dimension(:) :: estremo_m, estremo_m_tilde, CB, CC, CE, CF
-    double precision, device, allocatable, dimension(:) :: wint_d, xint_d, w_d, x_d 
+    double precision, constant, dimension(32) :: wint_d, xint_d, w_d, x_d 
     double precision, device, allocatable, dimension(:,:) :: punto_m_1, punto_m_2, punto_m_tilde_1, punto_m_tilde_2, CA, CD, CBCFCECC, CFCACCCD, &
                                                              CBCDCECA, CBCCCECF, CACCCFCD, CACBCDCE, Vu_d, matrix_d
-    logical, device, allocatable, dimension(:) :: IsULeftAssI, IsULeftAssJ
-    integer, device :: delta_kronecker_d(2,2), grado_q_d, DimVu_d, hk_d, NGauss_d
+    !logical, device, allocatable, dimension(:) :: IsULeftAssI, IsULeftAssJ
+    integer, constant :: delta_kronecker_d(2,2), grado_q_d, DimVu_d, hk_d, NGauss_d
     integer, device, allocatable, dimension(:,:) :: flag_extra, list_elements_d
 end module Variables_DP
